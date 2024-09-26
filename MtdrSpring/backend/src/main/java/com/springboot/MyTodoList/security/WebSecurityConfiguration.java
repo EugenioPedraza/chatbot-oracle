f35855bd255b74bb76f252a2210eb6aa67cef06b
EugenioPedraza <a00835064@tec.mx>
2024-09-26 10:44:36 -0600

    version con base de datos de george

--- MtdrSpring/backend/src/main/java/com/springboot/MyTodoList/security/WebSecurityConfiguration.java
+++ MtdrSpring/backend/src/main/java/com/springboot/MyTodoList/security/WebSecurityConfiguration.java
@@ -12,12 +12,6 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity httpSecurity) throws Exception {
         httpSecurity.csrf().disable();
-<<<<<<< HEAD
         httpSecurity.authorizeRequests().anyRequest().permitAll();
-=======
-        httpSecurity.authorizeRequests().anyRequest().//.authenticated().and().
-                //formLogin().and().logout()
-                permitAll();
->>>>>>> 76f51340eaa69acac2371286417a894f3c3aba8c
     }
 }

007384f77d1ca648d0c0c6932db129f7bde81719
Jorge Bustamante <94580142+jorgebustmonttec@users.noreply.github.com>
2024-10-08 12:38:36 -0600

    modificaciones modelo tarea
    regresa horas, fechainicio y fecha fin

--- MtdrSpring/backend/src/main/java/com/springboot/MyTodoList/service/TareaService.java
+++ MtdrSpring/backend/src/main/java/com/springboot/MyTodoList/service/TareaService.java
@@ -20,7 +20,7 @@ public class TareaService {
     }
 
     public Tarea getTareaById(int id) {
-        return tareaRepository.findById(id).get();
+        return tareaRepository.findById(id).orElse(null);
     }
 
     public Tarea addTarea(Tarea tarea) {
@@ -28,15 +28,22 @@ public class TareaService {
     }
 
     public Tarea updateTarea(int id, Tarea tarea) {
-        Tarea tarea2 = tareaRepository.findById(id).get();
-        tarea2.setIDTarea(id);
-        tarea2.setDescripcionTarea(tarea.getDescripcionTarea());
-        tarea2.setEstadoTarea(tarea.getEstadoTarea());
-        tarea2.setFechaAsignacion(tarea.getFechaAsignacion());
-        tarea2.setFechaVencimiento(tarea.getFechaVencimiento());
-        tarea2.setIDSprint(tarea.getIDSprint());
-        tarea2.setIDUsuario(tarea.getIDUsuario());
-        return tareaRepository.save(tarea2);
+        Tarea tarea2 = tareaRepository.findById(id).orElse(null);
+        if (tarea2 != null) {
+            tarea2.setIDTarea(id);
+            tarea2.setDescripcionTarea(tarea.getDescripcionTarea());
+            tarea2.setEstadoTarea(tarea.getEstadoTarea());
+            tarea2.setFechaAsignacion(tarea.getFechaAsignacion());
+            tarea2.setFechaVencimiento(tarea.getFechaVencimiento());
+            tarea2.setIDSprint(tarea.getIDSprint());
+            tarea2.setIDUsuario(tarea.getIDUsuario());
+            tarea2.setPuntos(tarea.getPuntos());
+            tarea2.setFechaInicio(tarea.getFechaInicio());
+            tarea2.setFechaFin(tarea.getFechaFin());
+            tarea2.setHoras(tarea.getHoras() != null ? tarea.getHoras() : 0); // Ensure horas is not null
+            return tareaRepository.save(tarea2);
+        }
+        return null;
     }
 
     public boolean deleteTarea(int id) {
@@ -47,5 +54,4 @@ public class TareaService {
             return false;
         }
     }
-
 }

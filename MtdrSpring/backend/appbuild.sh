32d0baca6ea3ffa2f9a06a4958d16756058eedb7
EugenioPedraza <a00835064@tec.mx>
2024-11-04 10:49:25 -0600

    cambios

--- MtdrSpring/backend/appbuild.sh
+++ MtdrSpring/backend/appbuild.sh
@@ -1,4 +1,11 @@
+<<<<<<< Updated upstream
 docker rm springapp
 mvn verify
 docker build -f Dockerfile --platform linux/amd64 -t todolistapp-springboot:0.1 .  
 docker run --name springapp -p 8080:8080 -d todolistapp-springboot:0.1
+=======
+docker rm springboot
+mvn verify
+docker build -f Dockerfile -t mx-queretaro-1.ocir.io/ax9ejkhbh4ic/reacttodo/lc4xp/todolistapp-springboot:0.1 .
+docker run -it --name springboot -p 8080:8080 mx-queretaro-1.ocir.io/ax9ejkhbh4ic/reacttodo/lc4xp/todolistapp-springboot:0.1
+>>>>>>> Stashed changes
